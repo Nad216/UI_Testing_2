30f8ecf8d19ab7d55764164a0ac0932581b5b7bb
Nadun Avishka <codse211p011@student.nibm.lk>
2022-04-03 18:54:22 +0530

    Add project files.

---
+++ UI_Testing_2/Color_codes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace UI_Testing_2
+{
+    class Color_codes
+    {
+
+        
+        public Brush cd_fill(string code)
+        {
+            var converter = new System.Windows.Media.BrushConverter();
+            var brush = (Brush)converter.ConvertFromString(code);
+            return brush;
+        }
+
+    }
+}
---
+++ UI_Testing_2/DashBoard1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI_Testing_2
+{
+    /// <summary>
+    /// Interaction logic for DashBoard1.xaml
+    /// </summary>
+    public partial class DashBoard1 : Window
+    {
+        DB_connetions log1 = new DB_connetions();
+        Color_codes ccg = new Color_codes();
+        public DashBoard1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
